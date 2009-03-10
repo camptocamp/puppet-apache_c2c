9b4d4878c5ff65f3296a17a0c46b5f312405ce8b
Marc Fournier <marc.fournier@camptocamp.com>
2009-03-10 14:28:24 +0100

    Added conditional run, based on $ensure + require File["${wwwroot}/${name}"]

--- manifests/definitions/vhost-ssl.pp
+++ manifests/definitions/vhost-ssl.pp
@@ -11,30 +11,32 @@ define apache::vhost-ssl ($ensure, $ip_address = "*", $htdocs=false, $conf=false
     mode           => $mode,
   }
 
-  file { "${wwwroot}/${name}/ssl":
-    ensure => directory,
-    owner  => "root",
-    group  => "root",
-    mode   => 700,
-    seltype => "cert_t",
-  }
+  if $ensure == "present" {
+    file { "${wwwroot}/${name}/ssl":
+      ensure => directory,
+      owner  => "root",
+      group  => "root",
+      mode   => 700,
+      seltype => "cert_t",
+      require => [File["${wwwroot}/${name}"]],
+    }
 
-  exec { "generate-ssl-cert-$name":
-    command => "/usr/local/sbin/generate-ssl-cert.sh $name /etc/ssl/ssleay.cnf ${wwwroot}/${name}/ssl/apache.pem",
-    creates => "${wwwroot}/${name}/ssl/apache.pem",
-    require => [
-      File["${wwwroot}/${name}/ssl"],
-      File["/usr/local/sbin/generate-ssl-cert.sh"],
-      File["/etc/ssl/ssleay.cnf"]
-    ],
-  }
+    exec { "generate-ssl-cert-$name":
+      command => "/usr/local/sbin/generate-ssl-cert.sh $name /etc/ssl/ssleay.cnf ${wwwroot}/${name}/ssl/apache.pem",
+      creates => "${wwwroot}/${name}/ssl/apache.pem",
+      require => [
+        File["${wwwroot}/${name}/ssl"],
+        File["/usr/local/sbin/generate-ssl-cert.sh"],
+        File["/etc/ssl/ssleay.cnf"]
+      ],
+    }
 
-  file { "${wwwroot}/${name}/ssl/apache.pem":
-    owner => "root",
-    group => "root",
-    mode  => 750,
-    seltype => "cert_t",
-    require => [File["${wwwroot}/${name}/ssl"], Exec["generate-ssl-cert-$name"]],
+    file { "${wwwroot}/${name}/ssl/apache.pem":
+      owner => "root",
+      group => "root",
+      mode  => 750,
+      seltype => "cert_t",
+      require => [File["${wwwroot}/${name}/ssl"], Exec["generate-ssl-cert-$name"]],
+    }
   }
-
 }
