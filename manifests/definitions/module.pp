bb9ffdccd285110ef604887d59425f4d7af10e3c
Mathieu Bornoz <mbornoz@ip-10-226-54-102.eu-west-1.compute.internal>
2009-03-10 11:15:53 +0100

    remove global attributes

--- manifests/definitions/module.pp
+++ manifests/definitions/module.pp
@@ -1,17 +1,24 @@
 define apache::module ($ensure='present') {
+  
+  $wwwconf = $operatingsystem ? {
+    Redhat  => "/etc/httpd",
+    Debian  => "/etc/apache2",
+    default => { notice "Unsupported operatingsystem ${operatingsystem}",
+  }
+
   case $ensure {
     'present' : {
-      exec { "/usr/sbin/a2enmod ${name}":
-        unless  => "/bin/sh -c '[ -L ${mods}-enabled/${name}.load ] \\
-          && [ ${mods}-enabled/${name}.load -ef ${mods}-available/${name}.load ]'",
+      exec { "a2enmod ${name}":
+        unless  => "/bin/sh -c '[ -L ${wwwconf}/${name}.load ] \\
+          && [ ${wwwconf}/mods-enabled/${name}.load -ef ${wwwconf}/mods-available/${name}.load ]'",
         require => Package["apache2"],
         notify  => Service["apache2"],
       }
     }
     'absent': {
-      exec { "/usr/sbin/a2dismod ${name}": 
-        onlyif  => "/bin/sh -c '[ -L ${mods}-enabled/${name}.load ] \\
-          && [ ${mods}-enabled/${name}.load -ef ${mods}-available/${name}.load ]'",
+      exec { "a2dismod ${name}": 
+        onlyif  => "/bin/sh -c '[ -L ${wwwconf}/mods-enabled/${name}.load ] \\
+          && [ ${wwwconf}/mods-enabled/${name}.load -ef ${wwwconf}/mods-available/${name}.load ]'",
         require => Package["apache2"],
         notify  => Service["apache2"],
        }
@@ -21,26 +28,3 @@ define apache::module ($ensure='present') {
     }
   }
 }
-
-#define apache::module ($ensure='present') {
-#  case $ensure {
-#    'present' : {
-#      exec { "a2enmod ${name}":
-#        unless  => "/bin/sh -c '[ -L $wwwconf/mods-enabled/${name}.load ] \\
-#          && [ $wwwconf/mods-enabled/${name}.load -ef $wwwconf/mods-available/${name}.load ]'",
-#        notify  => Service["$wwwpkgname"],
-#      }
-#    }
-#    'absent': {
-#      exec { "a2dismod ${name}": 
-#        onlyif  => "/bin/sh -c '[ -L $wwwconf/mods-enabled/${name}.load ] \\
-#          && [ $wwwconf/mods-enabled/${name}.load -ef $wwwconf/mods-available/${name}.load ]'",
-#        require => [Package["$wwwpkgname"], File["$wwwconf/mods-available"], File["$wwwconf/mods-enabled"]],
-#        notify  => Service["$wwwpkgname"],
-#       }
-#    }
-#    default: { 
-#      err ( "Unknown ensure value: '${ensure}'" ) 
-#    }
-#  }
-#}
