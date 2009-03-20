670feffe14039b7b2c8997c98da81f69ad9fc6c1
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-03-20 11:53:21 +0100

    changed nagios class

--- manifests/classes/apache-nagios.pp
+++ manifests/classes/apache-nagios.pp
@@ -1,6 +1,6 @@
 class apache::nagios {
 
-  nagios::service::distributed {"check_apachestatus":
+  nagios::service::distributed {"check_apachestatus!localhost!80":
     service_description => "check apache2 status",
   }
 
