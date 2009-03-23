53aa6e5257e7fc47d854a8018b5bc9f2a79fd00d
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-03-23 09:57:51 +0100

    port 443 is already defined in lenny

--- manifests/classes/apache-ssl-debian.pp
+++ manifests/classes/apache-ssl-debian.pp
@@ -4,12 +4,16 @@ class apache::ssl::debian inherits apache::base::ssl {
     ensure => present,
   }
 
-  line {"listen on port 443":
-    ensure => present,
-    line => "Listen 443",
-    file => "/etc/apache2/ports.conf",
-    notify  => Service["apache"],
-    require => Package["apache"],
+  case $lsbdistcodename {
+    etch: {
+      line {"listen on port 443":
+        ensure => present,
+        line => "Listen 443",
+        file => "/etc/apache2/ports.conf",
+        notify  => Service["apache"],
+        require => Package["apache"],
+      }
+    }
   }
 
 }
