4f575ceaa627c392039eb74de0aab121f11f9e4a
Marc Fournier <marc.fournier@camptocamp.com>
2009-05-13 12:55:01 +0200

    redhat: new definition to manage apache's selinux booleans

---
+++ manifests/definitions/redhat-selinux.pp
@@ -0,0 +1,54 @@
+define apache::redhat::selinux() {
+
+  case $name {
+
+    cgi: {
+      selboolean { "httpd_enable_cgi":
+        value => "on",
+        persistent => true,
+      }
+    }
+
+    userdir: {
+      selboolean { "httpd_enable_homedirs":
+        value => "on",
+        persistent => true,
+      }
+    }
+
+    proxy, rewrite, ldap, shib: {
+      if defined(Selboolean["httpd_can_network_connect"]) { }
+      else {
+        selboolean { "httpd_can_network_connect": # or httpd_can_network_relay ??
+          value => "on",
+          persistent => true,
+        }
+      }
+    }
+
+    php, perl: {
+      if defined(Selboolean["httpd_builtin_scripting"]) { }
+      else {
+        selboolean { ["httpd_builtin_scripting", "httpd_can_network_connect_db"]:
+          value => "on",
+          persistent => true,
+        }
+      }
+    }
+
+    suexec: {
+      selboolean { "httpd_suexec_disable_trans":
+        value => "off",
+        persistent => true,
+      }
+    }
+
+    include: {
+      selboolean { "httpd_ssi_exec":
+        value => "on",
+        persistent => true,
+      }
+    }
+
+  }
+}
