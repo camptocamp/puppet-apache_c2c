56be8837da2adfc1451e08664bbe3a7fce9a684c
Marc Fournier <marc.fournier@camptocamp.com>
2009-05-19 16:32:52 +0200

    merged 2 deflate files in 1 and added default path for redhat

--- manifests/classes/deflate.pp
+++ manifests/classes/deflate.pp
@@ -1,19 +1,30 @@
 class apache::deflate {
+
   apache::module {"deflate":
     ensure => present,
   }
 
-  file {"/etc/apache2/conf.d/deflate-javascript.conf":
-    ensure  => present,
-    content => "<IfModule mod_deflate.c>\nAddOutputFilterByType DEFLATE application/x-javascript application/javascript\nBrowserMatch Safari no-gzip\n</IfModule>\n",
+  #TODO: remove soon
+  file { ["/etc/apache2/conf.d/deflate-javascript.conf",
+          "/etc/apache2/conf.d/deflate-css.conf"]:
+    ensure  => absent,
+    require => File["deflate.conf"],
+  }
+
+  file { "deflate.conf":
+    ensure => present,
+    path => $operatingsystem ? {
+      RedHat => "/etc/httpd/conf.d/deflate.conf",
+      Debian => "/etc/apache2/conf.d/deflate.conf",
+    },
+    content => "# file managed by puppet
+<IfModule mod_deflate.c>
+  AddOutputFilterByType DEFLATE application/x-javascript application/javascript text/css
+  BrowserMatch Safari no-gzip
+</IfModule>
+",
     notify  => Service["apache"],
     require => Package["apache"],
   }
 
-  file {"/etc/apache2/conf.d/deflate-css.conf":
-    ensure  => present,
-    content => "<IfModule mod_deflate.c>\nAddOutputFilterByType DEFLATE text/css\n</IfModule>\n",
-    notify  => Service["apache"],
-    require => Package["apache"],
-  }
 }
