c3261d3be166ea7d1436fd05f3660eb47dc62a7d
Marc Fournier <marc.fournier@camptocamp.com>
2009-05-19 16:43:32 +0200

    new class apache::reverseproxy, to include one servers where ProxyPass is required

---
+++ manifests/classes/reverseproxy.pp
@@ -0,0 +1,24 @@
+class apache::reverseproxy {
+
+  apache::module {["proxy", "proxy_http", "proxy_ajp", "proxy_connect"]: }
+
+  file { "reverseproxy.conf":
+    ensure => "present",
+    path => $operatingsystem ? {
+      RedHat => "/etc/httpd/conf.d/reverseproxy.conf",
+      Debian => "/etc/apache2/conf.d/reverseproxy.conf",
+    },
+    content => "# file managed by puppet
+<IfModule mod_proxy.c>
+  ProxyRequests Off
+  <Proxy *>
+    Order Deny,Allow
+    Deny from all
+  </Proxy>
+</IfModule>
+",
+    notify  => Service["apache"],
+    require => Package["apache"],
+  }
+
+}
