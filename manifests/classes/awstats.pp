df361774d07f8ef2625b7fa7e44408ca7298007a
Marc Fournier <marc.fournier@camptocamp.com>
2009-05-26 21:11:41 +0200

    adapted awstats class & definition for redhat

--- manifests/classes/awstats.pp
+++ manifests/classes/awstats.pp
@@ -1,28 +1,57 @@
 class apache::awstats {
 
-  package{"awstats":
+  package { "awstats":
     ensure => installed
   }
 
-  file {"/etc/awstats/awstats.conf":
+  file { ["/etc/awstats/awstats.conf",
+         "/etc/awstats/awstats.conf.local",
+         "/etc/awstats/awstats.model.conf",
+         "/etc/awstats/awstats.localhost.localdomain.conf",
+         "/etc/awstats/awstats.${fqdn}.conf"]:
     ensure  => absent,
     require => Package[awstats]
   }
 
-  file {"/etc/awstats/awstats.conf.local":
-    ensure  => absent,
-    require => Package[awstats]
-  }
+  case $operatingsystem {
 
-  cron {"update all awstats virtual hosts":
-    command => "/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null",
-    user    => "root",
-    minute  => [0,10,20,30,40,50],
-    require => Package[awstats]
-  }
-  file{"/etc/cron.d/awstats":
-    ensure => absent,
-  }
+    Debian: {
+      cron { "update all awstats virtual hosts":
+        command => "/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null",
+        user    => "root",
+        minute  => [0,10,20,30,40,50],
+        require => Package[awstats]
+      }
 
+      file { "/etc/cron.d/awstats":
+        ensure => absent,
+      }
+    }
+
+    RedHat: {
+
+      # awstats RPM installs its own cron in /etc/cron.hourly/awstats
+
+      file { "/usr/share/awstats/wwwroot/cgi-bin/":
+        seltype => "httpd_sys_script_exec_t",
+        recurse => true,
+        require => Package["awstats"],
+      }
+
+      file { "/var/lib/awstats/":
+        seltype => "httpd_sys_script_ro_t",
+        recurse => true,
+        require => Package["awstats"],
+      }
+
+      file { "/etc/httpd/conf.d/awstats.conf":
+        ensure  => absent,
+        require => Package["awstats"],
+        notify  => Service["apache"],
+      }
+    }
+
+    default: { fail "Unsupported operatingsystem ${operatingsystem}" }
+  }
 
 }
--- manifests/definitions/aw-stats.pp
+++ manifests/definitions/aw-stats.pp
@@ -1,15 +1,29 @@
-define apache::aw-stats($ensure=present){
+define apache::aw-stats($ensure=present) {
+
+  case $operatingsystem {
+    redhat : {
+      $wwwroot = "/var/www/vhosts"
+      $conf = "awstats.rh.conf"
+    }
+    debian : {
+      $wwwroot = "/var/www"
+      $conf = "awstats.deb.conf"
+    }
+    default : { fail "Unsupported operatingsystem ${operatingsystem}" }
+  }
+
   file{"/etc/awstats/awstats.${name}.conf":
     ensure  => $ensure,
     content => template("apache/awstats.erb"),
-    require => Package["apache2"],
+    require => [Package["apache"], Class["apache::awstats"]],
   }
 
-  file{"/var/www/${name}/conf/awstats.conf":
+  file{"${wwwroot}/${name}/conf/awstats.conf":
     ensure  => $ensure,
     owner   => root,
     group   => root,
-    source  => "puppet:///apache/awstats.conf",
-    notify  => Service["apache2"],
+    source  => "puppet:///apache/${conf}",
+    notify  => Service["apache"],
+    require => File["${wwwroot}/${name}/conf"],
   }
 }
