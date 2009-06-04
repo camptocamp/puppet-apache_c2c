10ba4b1e92c2eb202dfe06956bae51837ed36b34
Marc Fournier <marc.fournier@camptocamp.com>
2009-06-04 22:34:01 +0200

    adapted apache::auth::ldap for redhat
     - takes redhat in account
     - reorganized parameters to behave like other apache::auth definitions
     - removed non-ldap stuff out of template
    
    TODO: test this on Debian/Ubuntu !

--- manifests/definitions/auth-ldap.pp
+++ manifests/definitions/auth-ldap.pp
@@ -1,11 +1,23 @@
-define apache::auth::ldap ($ensure, $vhost, $ldapUrl, $confname) {
+define apache::auth::ldap ($ensure="present", $authname="Private Area", $vhost, $ldapUrl, $location) {
 
-  $location = $name
+  case $operatingsystem {
+    redhat : { $wwwroot = "/var/www/vhosts" }
+    debian : { $wwwroot = "/var/www" }
+    default : { fail "Unsupported operatingsystem ${operatingsystem}" }
+  }
 
-  file {"/var/www/${vhost}/conf/${confname}.conf":
+  if defined(Apache::Module["ldap"]) {} else {
+    apache::module {"ldap": }
+  }
+
+  if defined(Apache::Module["authnz_ldap"]) {} else {
+    apache::module {"authnz_ldap": }
+  }
+
+  file {"${wwwroot}/${vhost}/conf/${name}.conf":
     ensure  => $ensure,
     content => template("apache/auth-ldap.erb"),
-    notify  => Service["apache2"],
+    notify  => Service["apache"],
   }
 
 }
