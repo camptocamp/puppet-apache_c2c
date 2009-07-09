b429d426f29920a9f9c0a374fc1703563245ec6c
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-07-09 13:51:14 +0200

    clean and improuve webdav definition

--- manifests/definitions/webdav-instance.pp
+++ manifests/definitions/webdav-instance.pp
@@ -1,55 +1,23 @@
-define apache::webdav::instance ($ensure=present, $vhost, $purge='5', $directory=false) {
+define apache::webdav::instance ($ensure=present, $vhost, $directory=false) {
 
   if $directory {
-    $davdir = "${directory}${name}"
+    $davdir = "${directory}/webdav-${name}"
   } else {
-    $davdir = "/var/www/${vhost}/private${name}" 
+    $davdir = "/var/www/${vhost}/private/webdav-${name}" 
   }
 
-  $clean_name = generate("/bin/bash","-c","/bin/echo -n ${name} | sed 's#/#-#g'")
-  notice "$name ->> $clean_name"
-
-  case $ensure {
-    'present' :{
-      exec {"Creating dav basedir for ${name}":
-        command => "mkdir -p $davdir",
-        unless => "test -d $davdir",
-        require => File["/var/www/${vhost}/conf/webdav${clean_name}.conf"],
-      }
-    }
-    'absent' :{
-      exec {"Removing dav basedir for ${name}":
-        command => "rm -rf $davdir",
-        onlyif => "test -d $davdir",
-        require => File["/var/www/${vhost}/conf/webdav${clean_name}.conf"],
-      }
-    }
+  file {$davdir:
+    ensure => directory,
+    owner => "www-data",
+    group => "www-data",
+    mode => 2755,
   }
 
   # configuration
-  file {"/var/www/${vhost}/conf/webdav${clean_name}.conf" :
+  file {"/var/www/${vhost}/conf/webdav-${name}.conf" :
     ensure => $ensure,
     content => template("apache/webdav-config.erb"),
-  }
-
-  # do we want to hold files for a very long time?
-  case $purge {
-    '0' :{ 
-       cron {"Clean old files from DAV $davdir":
-          command => "/usr/sbin/tmpreaper --ctime --all ${purge}d ${davdir}",
-          user    => "root",
-          ensure  => absent,
-        }
-    }
-    default : {
-       cron {"Clean old files from DAV $davdir":
-          command => "/usr/sbin/tmpreaper --ctime --all ${purge}d ${davdir}",
-          user    => "root",
-          minute  => 0,
-          hour    => 1,
-          ensure  => present,
-        }
-    }
+    require => File[$davdir],
   }
 
 }
