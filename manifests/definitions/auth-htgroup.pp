5a1a137782d5607500dd173a48e363917a4e27e9
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-07-09 13:54:42 +0200

    manage groups and users (mod_authz_user + mod_authz_groupfile)

---
+++ manifests/definitions/auth-htgroup.pp
@@ -0,0 +1,45 @@
+define apache::auth::htgroup (
+  $ensure="present", 
+  $vhost=false,
+  $groupFileLocation=false,
+  $groupFileName="htgroup",
+  $groupname,
+  $members){
+ 
+  if $groupFileLocation {
+    if defined(File[$groupFileLocation]) {
+      $_authGroupFile = "${groupFileLocation}/${groupFileName}"
+    } else {
+      fail "location $groupFileLocation is not defined !"
+    } 
+  } else {
+    if $vhost {
+      $_authGroupFile = "/var/www/${vhost}/private/${groupFileName}"
+    } else {
+      fail "parameter vhost is require !"
+    }
+  }
+  
+  case $ensure {
+
+    'present': {
+      exec {"! test -f $_authGroupFile && OPT='-c'; htgroup \$OPT $_authGroupFile $groupname $members":
+        unless => "grep -q $groupname $_authGroupFile",
+        require => File[$groupFileLocation],
+      }
+    }
+
+    'absent': {
+      exec {"htgroup -D $_authGroupFile $groupname":
+        onlyif => "grep -q $groupname $_authGroupFile",
+        notify => Exec["delete $_authGroupFile if empty"],
+      }
+
+      exec {"delete $_authGroupFile if empty":
+        command => "rm -f $_authGroupFile",
+        onlyif => "wc -l $_authGroupFile |grep -q 0",
+        refreshonly => true,
+      } 
+    }
+  }
+}
--- manifests/definitions/auth-htpasswd.pp
+++ manifests/definitions/auth-htpasswd.pp
@@ -1,5 +1,63 @@
-define apache::auth::htpasswd ($ensure=present, $file, $username, $password) {
-  exec {"! test -f $file && OPT='-c'; htpasswd -b \$OPT $file $username $password":
-    unless  => "grep -q $username $file",
+define apache::auth::htpasswd (
+  $ensure="present", 
+  $vhost=false,
+  $userFileLocation=false,
+  $userFileName="htpasswd",
+  $username,
+  $md5Password=false,
+  $clearPassword=false){
+ 
+  if $userFileLocation {
+    if defined(File[$userFileLocation]) {
+      $_authUserFile = "${userFileLocation}/${userFileName}"
+    } else {
+      fail "location $userFileLocation is not defined !"
+    } 
+  } else {
+    if $vhost {
+      $_authUserFile = "/var/www/${vhost}/private/${userFileName}"
+    } else {
+      fail "parameter vhost is require !"
+    }
+  }
+  
+  case $ensure {
+
+    'present': {
+      if $md5Password and $clearPassword {
+        fail "choose only one of md5Password OR clearPassword !"
+      }
+
+      if !$md5Password and !$clearPassword  {
+        fail "choose one of md5Password OR clearPassword !"
+      }
+
+      if $md5Password {
+        exec {"! test -f $_authUserFile && OPT='-c'; htpasswd -bp \$OPT $_authUserFile $username '$md5Password'":
+          unless => "grep -q $username $_authUserFile",
+          require => File[$userFileLocation],
+        }
+      }
+
+      if $clearPassword {
+        exec {"! test -f $_authUserFile && OPT='-c'; htpasswd -bm \$OPT $_authUserFile $username $clearPassword":
+          unless => "grep -q $username $_authUserFile",
+          require => File[$userFileLocation],
+        }
+      }
+    }
+
+    'absent': {
+      exec {"htpasswd -D $_authUserFile $username":
+        onlyif => "grep -q $username $_authUserFile",
+        notify => Exec["delete $_authUserFile if empty"],
+      }
+
+      exec {"delete $_authUserFile if empty":
+        command => "rm -f $_authUserFile",
+        onlyif => "wc -l $_authUserFile |grep -q 0",
+        refreshonly => true,
+      } 
+    }
   }
 }
