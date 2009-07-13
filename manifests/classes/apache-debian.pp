31e677f510b7c5c635167ff28e0f84dd5ab3ea93
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-07-13 12:18:15 +0200

    moved NameVirtualHost directive in default file and added a default ports file

--- manifests/classes/apache-debian.pp
+++ manifests/classes/apache-debian.pp
@@ -36,40 +36,38 @@ class apache::debian inherits apache::base {
     force => true,
   }
 
-  case $lsbdistcodename {
-    lenny: {
-      File["default virtualhost"] {
-        source => "puppet:///apache/etc/apache2/sites-available/default-lenny",
-      }
-    }
-  }
-
   file {"/var/www/index.html":
     ensure => absent,
   }
+
   file {"/var/www/html":
     ensure => directory,
     require => File["/var/www"],
   }
+
   file {"/var/www/html/index.html":
     ensure  => present,
-    owner   => "root",
-    group   => "root",
+    owner   => root,
+    group   => root,
     mode    => 644,
     content => "<html><body><h1>It works!</h1></body></html>\n",
     require => File["/var/www/html"],
   }
 
-  file { "/etc/apache2/conf.d/servername.conf":
+  file {"/etc/apache2/conf.d/servername.conf":
     content => "ServerName ${fqdn}\n",
     notify  => Service["apache"],
     require => Package["apache"],
   }
-
-  #TODO: remove once deployed everywhere
-  exec { "sed -i '/^ServerName/d' /etc/apache2/ports.conf":
-    onlyif  => "grep -q ServerName /etc/apache2/ports.conf",
+  
+  file {"/etc/apache2/ports.conf":
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 644,
+    source => "puppet:///apache/etc/apache2/ports.conf",
     notify  => Service["apache"],
     require => Package["apache"],
   }
+    
 }
