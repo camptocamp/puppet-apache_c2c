ef6b9385f0691baae6a15f543bd9d66f5732b522
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-07-13 15:08:12 +0200

    not used

--- manifests/classes/no_default_site.pp
+++
@@ -1,6 +0,0 @@
-class apache::no_default_site inherits apache::base {
-  exec { "/usr/sbin/a2dissite default":
-    onlyif => "/usr/bin/test -L /etc/apache2/sites-enabled/000-default",
-    notify => Exec["apache2-graceful"],
-  }
-}
