c6df7fcedd08065b6a7299b41e4099adfd1b8b6b
Mathieu Bornoz <mbornoz@www19.epfl.ch>
2009-07-22 15:21:47 +0200

    added support for LDAP authentication

---
+++ manifests/definitions/auth-basic-ldap.pp
@@ -0,0 +1,35 @@
+define apache::auth::basic::ldap (
+  $ensure="present", 
+  $authname="Private Area",
+  $vhost,
+  $location="/",
+  $authLDAPUrl,
+  $authLDAPBindDN=false,
+  $authLDAPBindPassword=false,
+  $authLDAPCharsetConfig=false,
+  $authLDAPCompareDNOnServer=false,
+  $authLDAPDereferenceAliases=false,
+  $authLDAPGroupAttribute=false,
+  $authLDAPGroupAttributeIsDN=false,
+  $authLDAPRemoteUserAttribute=false,
+  $authLDAPRemoteUserIsDN=false,
+  $authzLDAPAuthoritative=false,
+  $authzRequire="valid-user"){
+
+  case $operatingsystem {
+    redhat : { $wwwroot = "/var/www/vhosts" }
+    debian : { $wwwroot = "/var/www" }
+    default : { fail "Unsupported operatingsystem ${operatingsystem}" }
+  }
+
+  if defined(Apache::Module["mod_authnz_ldap"]) {} else {
+    apache::module {"authnz_ldap": }
+  }
+
+  file {"${wwwroot}/${vhost}/conf/auth-basic-ldap-${name}.conf":
+    ensure => $ensure,
+    content => template("apache/auth-basic-ldap.erb"),
+    notify => Exec["apache-graceful"],
+  }
+
+}
