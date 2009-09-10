45f38f90bc6d367deb5ae827e54c15f4c020a052
Marc Fournier <marc.fournier@camptocamp.com>
2009-09-10 12:02:48 +0200

    apache::dev new class used to install apxs tool

---
+++ manifests/classes/apache-dev.pp
@@ -0,0 +1,21 @@
+#
+# == Class: apache::dev
+#
+# Installs package(s) required to build apache modules using apxs.
+#
+# Limitation: currently only works on redhat.
+#
+# Example usage:
+#
+#   include apache::dev
+#
+class apache::dev {
+
+  package { "apache-devel":
+    name    => $operatingsystem ? {
+      RedHat => "httpd-devel",
+    },
+    ensure  => present,
+    require => Package["gcc"],
+  }
+}
