c5a212ddb4e09c607f3dacc6e02baa021c53c97f
Marc Fournier <marc.fournier@camptocamp.com>
2009-10-16 13:10:33 +0200

    apache: generate-ssl-cert.sh error handling improvements

--- files/generate-ssl-cert.sh
+++ files/generate-ssl-cert.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # file managed by puppet
 
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 hostname template outputdir"
+  exit 1
+fi
+
 hostname=$1
 template=$2
 outputdir=$3
@@ -10,6 +15,12 @@ TMPFILE=`mktemp` || exit 1
 sed -e s#@HostName@#"$hostname"# $template > $TMPFILE
 
 export RANDFILE=/dev/random
-openssl req -config $TMPFILE -new -x509 -nodes -days 3650 -out $outputdir/$hostname.crt -keyout $outputdir/$hostname.key
-chmod 600 $outputdir/$hostname.key
-openssl req -new -key $outputdir/$hostname.key -config $TMPFILE > $outputdir/$hostname.csr
+
+if [ ! -e $outputdir/$hostname.crt ] || [ ! -e $outputdir/$hostname.key ]; then
+  openssl req -config $TMPFILE -new -x509 -nodes -days 3650 -out $outputdir/$hostname.crt -keyout $outputdir/$hostname.key || exit 1
+  chmod 600 $outputdir/$hostname.key
+fi
+
+openssl req -new -key $outputdir/$hostname.key -config $TMPFILE > $outputdir/$hostname.csr || exit 1
+
+rm -f $TMPFILE || exit 0
