a6d679065300b79ba5ba61cbe0ce90c9a9c2449a
Marc Fournier <marc.fournier@camptocamp.com>
2009-10-19 11:49:04 +0200

    apache: documentation for apache::ssl and apache::vhost-ssl

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,21 @@
 import "classes/*.pp"
 import "definitions/*.pp"
 
+/*
+
+== Class: apache
+
+Installs apache, ensures a few useful modules are installed (see apache::base),
+ensures that the service is running and the logs get rotated.
+
+By including subclasses where distro specific stuff is handled, it ensure that
+the apache class behaves the same way on diffrent distributions.
+
+Example usage:
+
+  include apache
+
+*/
 class apache {
   case $operatingsystem {
     Debian:  { include apache::debian}
@@ -9,6 +24,37 @@ class apache {
   }
 }
 
+/*
+
+== Class: apache::ssl
+
+This class basically does the same thing the "apache" class does + enable
+mod_ssl.
+
+It also drops a little shell script in /usr/local/sbin/generate-ssl-cert.sh,
+which is used by apache::vhost-ssl to generate an SSL key and certificate. This
+script calls openssl with /etc/ssl/ssleay.cnf as a template. The content of
+this file is influenced by a few class variables described below.
+
+Class variables:
+- *sslcert_country*: the content of the "countryName" field in generated
+  certificates. Setting this field is mandatory.
+- *sslcert_state*: the content of the "stateOrProvinceName" field in generated
+  certificates.
+- *sslcert_locality*: the content of the "localityName" field in generated
+  certificates.
+- *sslcert_organisation*:  the content of the "organizationName" field in
+  generated certificates. Setting this field is mandatory.
+- *sslcert_unit*: the content of the "organizationalUnitName" field in
+  generated certificates.
+- *sslcert_email*: the content of the "emailAddress" field in generated
+  certificates.
+
+Example usage:
+
+  include apache::ssl
+
+*/
 class apache::ssl inherits apache {
   case $operatingsystem {
     Debian:  { include apache::ssl::debian}
