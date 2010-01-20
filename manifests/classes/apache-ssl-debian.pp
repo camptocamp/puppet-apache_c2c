dca379785227ad9c3cc0d3371b570495978c903a
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2010-01-20 11:10:03 +0100

    Remove ancient 'Listen 443' directive
    This Listen 443 directive is already set in the default ports.conf and setting
    it twice prevents Apache from starting up.

--- manifests/classes/apache-ssl-debian.pp
+++ manifests/classes/apache-ssl-debian.pp
@@ -4,18 +4,6 @@ class apache::ssl::debian inherits apache::base::ssl {
     ensure => present,
   }
 
-  case $lsbdistcodename {
-    etch: {
-      line {"listen on port 443":
-        ensure => present,
-        line => "Listen 443",
-        file => "/etc/apache2/ports.conf",
-        notify  => Service["apache"],
-        require => Package["apache"],
-      }
-    }
-  }
-
   if !defined(Package["ca-certificates"]) {
     package { "ca-certificates":
       ensure => present,
