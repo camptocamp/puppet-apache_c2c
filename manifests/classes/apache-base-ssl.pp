8990628ca587549a99002d4f890a96c10adf2834
Marc Fournier <marc.fournier@camptocamp.com>
2010-02-11 18:38:58 +0100

    apache: accidental regression in d7842826.
    This was already removed once in 2f4a29ef.

--- manifests/classes/apache-base-ssl.pp
+++ manifests/classes/apache-base-ssl.pp
@@ -11,12 +11,8 @@ class apache::base::ssl {
 
   if $apache_ssl_ports {} else { $apache_ssl_ports = [443] }
 
-  if $sslcert_country {} else { $sslcert_country = "??" }
-  if $sslcert_state {} else { $sslcert_state = "undefined state" }
-  if $sslcert_locality {} else { $sslcert_locality = "undefined locality" }
-  if $sslcert_organisation {} else { $sslcert_organisation = "undefined organisation" }
-  if $sslcert_unit {} else { $sslcert_unit = "undefined unit" }
-  if $sslcert_email {} else { $sslcert_email = "undefined email" }
+  if !$sslcert_country { $sslcert_country = "??" }
+  if !$sslcert_organisation { $sslcert_organisation = "undefined organisation" }
 
   file { "/etc/ssl/":
     ensure => directory,
