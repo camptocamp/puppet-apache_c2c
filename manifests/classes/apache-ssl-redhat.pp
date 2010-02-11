camptocamp/puppet-apache_c2c

commit d7842826f657343049a9a4244964ecf88401815c

Added configuration variables for apache ports and parameters for vhost ports.

manifests/classes/apache-ssl-redhat.pp
@@ -18,4 +18,15 @@ class apache::ssl::redhat inherits apache::base::ssl {
     before => Exec["apache-graceful"],
   }
 
+  if $lsbmajdistrelease == 5 {
+    file {"/etc/httpd/mods-available/ssl.load":
+      ensure => present,
+      source => template("apache/ssl.load.erb"),
+      mode => 644,
+      owner => "root",
+      group => "root",
+      seltype => "httpd_config_t",
+      require => File["/etc/httpd/mods-available"],
+    }
+  }
 }

files/etc/apache2/ports.conf
@@ -1,9 +1,0 @@
-# file managed by puppet
-
-Listen 80
-
-<IfModule mod_ssl.c>
-    # SSL name based virtual hosts are not yet supported, therefore no
-    # NameVirtualHost statement here
-    Listen 443
-</IfModule>

manifests/classes/apache-base-ssl.pp
@@ -1,7 +1,13 @@
 class apache::base::ssl {
 
-  if !$sslcert_country { $sslcert_country = "??" }
-  if !$sslcert_organisation { $sslcert_organisation = "undefined organisation" }
+  if $apache_ssl_ports {} else { $apache_ssl_ports = [443] }
+
+  if $sslcert_country {} else { $sslcert_country = "??" }
+  if $sslcert_state {} else { $sslcert_state = "undefined state" }
+  if $sslcert_locality {} else { $sslcert_locality = "undefined locality" }
+  if $sslcert_organisation {} else { $sslcert_organisation = "undefined organisation" }
+  if $sslcert_unit {} else { $sslcert_unit = "undefined unit" }
+  if $sslcert_email {} else { $sslcert_email = "undefined email" }
 
   file { "/etc/ssl/":
     ensure => directory,

manifests/classes/apache-base.pp
@@ -1,5 +1,7 @@
 class apache::base {
 
+  if $apache_ports {} else { $apache_ports = [80] }
+
   file {"root directory":
     path => "/var/www",
     ensure => directory,
@@ -55,7 +57,7 @@ class apache::base {
   file {"default virtualhost":
     path => "/etc/apache2/sites-available/default",
     ensure => present,
-    source => "puppet:///apache/etc/apache2/sites-available/default",
+    content => template("apache/default-vhost"),
     require => Package["apache"],
     notify => Exec["apache-graceful"],
     mode => 644,

manifests/classes/apache-debian.pp
@@ -59,16 +59,24 @@ class apache::debian inherits apache::base {
     notify  => Service["apache"],
     require => Package["apache"],
   }
-  
-  file {"/etc/apache2/ports.conf":
-    ensure => present,
-    owner => root,
-    group => root,
-    mode => 644,
-    source => "puppet:///apache/etc/apache2/ports.conf",
-    notify  => Service["apache"],
+
+  common::concatfilepart {"apache.ports":
+    ensure  => present,
+    file    => "/etc/apache2/ports.conf",
+    content => template("apache/ports.conf.erb"),
     require => Package["apache"],
+    notify  => Service["apache"],
   }
+
+  #file {"/etc/apache2/ports.conf":
+  #  ensure => present,
+  #  owner => root,
+  #  group => root,
+  #  mode => 644,
+  #  source => "puppet:///apache/etc/apache2/ports.conf",
+  #  notify  => Service["apache"],
+  #  require => Package["apache"],
+  #}
   
   file {"/etc/apache2/sites-available/default-ssl":
     ensure => absent,

manifests/classes/apache-redhat.pp
@@ -22,7 +22,7 @@ class apache::redhat inherits apache::base {
 
   File["default virtualhost"] { 
     path => "/etc/httpd/sites-available/default",
-    source => "puppet:///apache/etc/httpd/sites-available/default",
+    content => template("apache/default-vhost.redhat"),
     seltype => "httpd_config_t",
   }  
   # END inheritance from apache::base
@@ -72,7 +72,7 @@ class apache::redhat inherits apache::base {
 
   file { "/etc/httpd/conf/httpd.conf":
     ensure => present,
-    source => "puppet:///apache/etc/httpd/conf/httpd.conf",
+    content => template("apache/httpd.conf.erb"),
     seltype => "httpd_config_t",
     notify  => Service["apache"],
     require => Package["apache"],
@@ -80,6 +80,7 @@ class apache::redhat inherits apache::base {
 
   # the following command was used to generate the content of the directory:
   # egrep '(^|#)LoadModule' /etc/httpd/conf/httpd.conf | sed -r 's|#?(.+ (.+)_module .+)|echo "\1" > mods-available/redhat5/\2.load|' | sh
+  # ssl.load was then changed to a template (see apache-redhat-ssl.pp)
   file {"/etc/httpd/mods-available":
     ensure => directory,
     source => $lsbmajdistrelease ? {

manifests/classes/apache-ssl-debian.pp
@@ -4,6 +4,14 @@ class apache::ssl::debian inherits apache::base::ssl {
     ensure => present,
   }
 
+  common::concatfilepart {"apache.ports.ssl":
+    ensure  => present,
+    file    => "/etc/apache2/ports.conf",
+    content => template("apache/ports.ssl.conf.erb"),
+    require => Package["apache"],
+    notify  => Service["apache"],
+  }
+
   if !defined(Package["ca-certificates"]) {
     package { "ca-certificates":
       ensure => present,

manifests/definitions/vhost-ssl.pp
@@ -46,6 +46,10 @@ Parameters:
 - *$sslonly*: if set to "true", only the https virtualhost will be configured.
   Defaults to "true", which means the virtualhost will be reachable unencrypted
   on port 80, as well as encrypted on port 443.
+- *ports*: array specifying the ports on which the vhost will be reachable.
+  Defaults to "all", which means all ports specified in $apache_ssl_ports (443
+  if not defined) and, if $sslonly is "false" (the default), $apache_ports (80
+  if not defined).
 
 Requires:
 - Class["apache-ssl"]
@@ -96,8 +100,11 @@ define apache::vhost-ssl (
   $certchain=false,
   $days="3650",
   $publish_csr=false,
-  $sslonly=false
+  $sslonly=false,
+  $ports="all"
 ) {
+
+  if $apache_ssl_ports {} else { $apache_ssl_ports = [443] }
 
   # define distro-specific paths and users.
   case $operatingsystem {
@@ -162,7 +169,8 @@ define apache::vhost-ssl (
     enable_default => $sslonly ? {
       true => false,
       default => true,
-    }
+    },
+    ports          => $ports,
   }
 
   if $sslonly {

commit 05ebe249c78d0129f5c980faf7d02eabcf05b5cd

apache: small error introduced in d7842826.

manifests/classes/apache-ssl-redhat.pp
@@ -21,7 +21,7 @@ class apache::ssl::redhat inherits apache::base::ssl {
   if $lsbmajdistrelease == 5 {
     file {"/etc/httpd/mods-available/ssl.load":
       ensure => present,
-      source => template("apache/ssl.load.erb"),
+      content => template("apache/ssl.load.erb"),
       mode => 644,
       owner => "root",
       group => "root",