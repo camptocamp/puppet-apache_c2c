b48a28396759a9ad0d4605570e0e08401a2c909e
Marc Fournier <marc.fournier@camptocamp.com>
2010-03-12 17:33:21 +0100

    apache::proxypass - documented definition usage.

--- manifests/definitions/proxypass.pp
+++ manifests/definitions/proxypass.pp
@@ -1,3 +1,33 @@
+/*
+
+== Definition: apache::proxypass
+
+Simple way of defining a proxypass directive for a given virtualhost.
+
+This definition will ensure all the required modules are loaded and will
+drop a configuration snippet in the virtualhost's conf/ directory.
+
+Parameters:
+- *ensure*: present/absent.
+- *location*: path in virtualhost's context to pass through using the ProxyPass
+  directive.
+- *url*: destination to which the ProxyPass directive points to.
+- *vhost*: the virtualhost to which this directive will apply. Mandatory.
+
+Requires:
+- Class["apache"]
+- matching Apache::Vhost[] instance
+
+Example usage:
+
+  apache::proxypass { "proxy legacy dir to legacy server":
+    ensure   => present,
+    location => "/legacy/",
+    url      => "http://legacyserver.example.com",
+    vhost    => "www.example.com",
+  }
+
+*/
 define apache::proxypass ($ensure="present", $location="", $url="", $vhost) {
 
   $fname = regsubst($name, "\s", "_", "G")
