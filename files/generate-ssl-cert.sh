c8a06f305f73e3ea3b21c06540e6d9d681d3f7ca
Marc Fournier <marc.fournier@camptocamp.com>
2010-03-26 17:06:15 +0100

    apache::vhost-ssl - added option to change the CN of the ssl certificate.

--- files/generate-ssl-cert.sh
+++ files/generate-ssl-cert.sh
@@ -11,17 +11,12 @@ template=$2
 outputdir=$3
 days=$4
 
-TMPFILE=`mktemp` || exit 1
-
-sed -e s#@HostName@#"$hostname"# $template > $TMPFILE
-
 export RANDFILE=/dev/random
 
 if [ ! -e $outputdir/$hostname.crt ] || [ ! -e $outputdir/$hostname.key ]; then
-  openssl req -config $TMPFILE -new -x509 -nodes -days $days -out $outputdir/$hostname.crt -keyout $outputdir/$hostname.key || exit 1
+  openssl req -config $template -new -x509 -nodes -days $days -out $outputdir/$hostname.crt -keyout $outputdir/$hostname.key || exit 1
   chmod 600 $outputdir/$hostname.key
 fi
 
-openssl req -new -key $outputdir/$hostname.key -config $TMPFILE > $outputdir/$hostname.csr || exit 1
+openssl req -new -key $outputdir/$hostname.key -config $template > $outputdir/$hostname.csr || exit 1
 
-rm -f $TMPFILE || exit 0
