c092ce16660a1948ca4683f37101174962e3b733
Marc Fournier <marc.fournier@camptocamp.com>
2010-08-03 17:10:36 +0200

    apache: removed moinmoin definitions. Belong to moinmoin module.

--- manifests/definitions/moinmoin-base.pp
+++
@@ -1,9 +0,0 @@
-define apache::moinmoin::base ($ensure=ensure) {
-
-  file {"/var/www/${name}/conf/wiki-base.conf":
-    ensure  => $ensure,
-    content => template("apache/moinmoin-base.erb"),
-    notify  => Exec["apache-graceful"],
-  }
-
-}
--- manifests/definitions/moinmoin-instance.pp
+++
@@ -1,9 +0,0 @@
-define apache::moinmoin::instance ($ensure=ensure,$vhost) {
-
-  file {"/var/www/${vhost}/conf/wiki-${name}.conf":
-    ensure  => $ensure,
-    content => template("apache/moinmoin-instance.erb"),
-    notify  => Exec["apache-graceful"],
-  }
-
-}
