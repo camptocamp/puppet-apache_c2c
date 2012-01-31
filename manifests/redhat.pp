172e1255aa74f5438e43e755296c45558e20828d
Justin Ellison <justin.ellison@buckle.com>
2012-01-31 10:38:47 -0600

    Should not rely on PATH being set.

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -4,8 +4,8 @@ class apache::redhat inherits apache::base {
   
   # BEGIN inheritance from apache::base
   Exec["apache-graceful"] {
-    command => "apachectl graceful",
-    onlyif  => "apachectl configtest",
+    command => "/usr/sbin/apachectl graceful",
+    onlyif  => "/usr/sbin/apachectl configtest",
   }
 
   Package["apache"] {
