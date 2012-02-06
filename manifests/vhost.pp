69fb7af56ac14ccbbbca06277ca2cde56093f4db
Rafael Felix Correa <rafael.correa@abril.com.br>
2012-02-06 18:53:13 -0200

    fixed path var to test -L command on vhost.pp

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -46,6 +46,7 @@ define apache::vhost (
       unless  => "test -L ${apache::params::conf}/sites-enabled/000-default",
       notify  => Exec["apache-graceful"],
       require => Package["apache"],
+      path    => ['/usr/bin', '/bin'],
     }
 
   } else {
