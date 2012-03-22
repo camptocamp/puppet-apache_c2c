3cfe36064a77ec708f08be8f3272fb2c56f8311a
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-03-22 12:35:17 +0100

    apache::confd - wrapper around apache::conf to put global configurations

---
+++ manifests/confd.pp
@@ -0,0 +1,35 @@
+/*
+
+== Definition: apache::confd
+
+Convenient wrapper around apache::conf definition to put configuration snippets in
+${apache::params::conf}/conf.d directory
+
+Parameters:
+- *ensure*: present/absent.
+- *configuration*: apache configuration(s) to be applied 
+- *filename*: basename of the file in which the configuration(s) will be put.
+  Useful in the case configuration order matters: apache reads the files in conf.d/
+  in alphabetical order.
+
+Requires:
+- Class["apache"]
+
+Example usage:
+
+  apache::confd { "example 1":
+    ensure        => present,
+    path          => /var/www/foo/conf
+    configuration => "WSGIPythonEggs /var/cache/python-eggs",
+  }
+
+*/
+define apache::confd($ensure=present,$configuration,$filename="") {
+  include apache::params
+  apache::conf {$name:
+    ensure        => $ensure,
+    path          => "${apache::params::conf}/conf.d",
+    filename      => $filename,
+    configuration => $configuration,
+  }
+}
