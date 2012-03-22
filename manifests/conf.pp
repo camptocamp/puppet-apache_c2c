d38f229b7829670d2d6001d5b2dcd2056f627cf5
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-03-22 12:34:27 +0100

    apache::conf - new definition to manage all apache configuration (in vhosts, and global)

---
+++ manifests/conf.pp
@@ -0,0 +1,47 @@
+/*
+
+== Definition: apache::conf
+
+Convenient wrapper around File[] resources to add random configuration
+snippets to apache. Shouldn't be called directly - please see apache::confd and apache::directive
+
+Parameters:
+- *ensure*:        present/absent.
+- *configuration*: apache configuration(s) to be applied 
+- *filename*:      basename of the file in which the configuration(s) will be put.
+                   Useful in the case configuration order matters: apache reads the files in conf.d/
+                   in alphabetical order.
+- *prefix*:        filename prefix
+- *path*:          directory for the file
+
+Requires:
+- Class["apache"]
+
+Example usage:
+
+  apache::conf { "example 1":
+    ensure        => present,
+    path          => /var/www/foo/conf
+    configuration => "WSGIPythonEggs /var/cache/python-eggs",
+  }
+
+*/
+define apache::conf($ensure=present, $filename="", $prefix="configuration", $configuration, $path) {
+  $fname = regsubst($name, "\s", "_", "G")
+
+  file{ "${name} configuration in ${path}":
+    ensure => $ensure,
+    content => "# file managed by puppet\n${configuration}\n",
+    seltype => $operatingsystem ? {
+      "RedHat" => "httpd_config_t",
+      "CentOS" => "httpd_config_t",
+      default  => undef,
+    },
+    name      => $filename ? {
+      ""      => "${path}/${prefix}-${fname}.conf",
+      default => "${path}/${filename}",
+    },
+    notify  => Service["apache"],
+  }
+
+}
