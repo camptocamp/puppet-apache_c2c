8f1b2ef765aeb52e3133c250ac17b585296fea0b
Raphaël Pinson <raphink@gmail.com>
2012-07-24 16:19:14 +0200

    Fix and lint apache::balancer

--- spec/defines/apache_balancer_spec.rb
+++ spec/defines/apache_balancer_spec.rb
@@ -15,16 +15,16 @@ describe 'apache::balancer' do
 
       it { should contain_apache__module('proxy') }
       it { should contain_apache__module('proxy_balancer') }
-      describe 'When using defaults' do
+      describe 'using defaults' do
 
         it { should contain_apache__module('proxy_http') }
         it do should contain_file('my balanced service balancer on default.org').with(
-          'ensure' => 'present',
-          'name'   => "#{VARS[os]['root']}/www.example.com/conf/balancer-my_balanced_service.conf"
+          :ensure => 'present',
+          :path   => "#{VARS[os]['root']}/default.org/conf/balancer-my_balanced_service.conf"
         ) end
       end
 
-      describe 'When using example usage' do
+      describe 'using example usage' do
         let(:params) { {
           :ensure     => 'present',
           :location   => '/mywebapp/',
@@ -42,9 +42,37 @@ describe 'apache::balancer' do
 
         it { should contain_apache__module('proxy_ajp') }
         it do should contain_file('my balanced service balancer on www.example.com').with(
-          'ensure' => 'present',
-          'name'   => "#{VARS[os]['root']}/www.example.com/conf/balancer1.conf"
+          :ensure => 'present',
+          :path   => "#{VARS[os]['root']}/www.example.com/conf/balancer1.conf"
         ) end
+      end
+
+      describe 'ensuring absent' do
+        let(:params) { {
+          :ensure => 'absent',
+          :vhost  => 'www.example.com',
+        } }
+
+        it { should include_class('apache::params') }
+
+        it { should contain_apache__module('proxy').with_ensure('absent') }
+        it { should contain_apache__module('proxy_balancer').with_ensure('absent') }
+        it { should contain_apache__module('proxy_http').with_ensure('absent') }
+        it { should contain_file('my balanced service balancer on www.example.com').with_ensure('absent') }
+      end
+
+      describe 'passing wrong proto' do
+        let(:params) { {
+          :ensure => 'present',
+          :vhost  => 'www.example.com',
+          :proto  => 'foobar',
+        } }
+
+        it do
+          expect {
+            should include_class('apache::params')
+          }.to raise_error(Puppet::Error, /foo/)
+        end
       end
     end
   end
