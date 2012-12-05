743a4fcf857ea0ba8774a9b59884b02b2ee22919
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-05 11:05:40 +0100

    lint modules/apache/manifests/ssl/redhat.pp

--- manifests/ssl/redhat.pp
+++ manifests/ssl/redhat.pp
@@ -1,34 +1,34 @@
 class apache::ssl::redhat inherits apache::base::ssl {
 
-  package {"mod_ssl":
+  package {'mod_ssl':
     ensure => installed,
   }
 
-  file {"/etc/httpd/conf.d/ssl.conf":
-    ensure => absent,
-    require => Package["mod_ssl"],
-    notify => Service["apache"],
-    before => Exec["apache-graceful"],
+  file {'/etc/httpd/conf.d/ssl.conf':
+    ensure  => absent,
+    require => Package['mod_ssl'],
+    notify  => Service['apache'],
+    before  => Exec['apache-graceful'],
   }
 
-  apache::module { "ssl":
-    ensure => present,
-    require => File["/etc/httpd/conf.d/ssl.conf"],
-    notify => Service["apache"],
-    before => Exec["apache-graceful"],
+  apache::module { 'ssl':
+    ensure  => present,
+    require => File['/etc/httpd/conf.d/ssl.conf'],
+    notify  => Service['apache'],
+    before  => Exec['apache-graceful'],
   }
 
-	case $::lsbmajdistrelease {
+  case $::lsbmajdistrelease {
     5,6: {
-      file {"/etc/httpd/mods-available/ssl.load":
-        ensure => present,
+      file {'/etc/httpd/mods-available/ssl.load':
+        ensure  => present,
         content => template("apache/ssl.load.rhel${::lsbmajdistrelease}.erb"),
-        mode => 644,
-        owner => "root",
-        group => "root",
-        seltype => "httpd_config_t",
-        require => File["/etc/httpd/mods-available"],
-			}
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        seltype => 'httpd_config_t',
+        require => File['/etc/httpd/mods-available'],
+      }
     }
   }
 }
