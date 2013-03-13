fbd61ac4aa11ad4f88cea48efb61112c2b355ffa
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-03-13 10:55:13 +0100

    Lint apache::vhost-ssl

--- manifests/vhost-ssl.pp
+++ manifests/vhost-ssl.pp
@@ -1,30 +1,26 @@
-/*
+# == Definition: apache::vhost-ssl
+#
+# This definition is deprecated and was replaced
+# by apache::vhost::ssl.
+#
+# The definition now wraps around apache::vhost::ssl
+# for backward compatibility reasons.
 
-== Definition: apache::vhost-ssl
-
-This definition is deprecated and was replaced
-by apache::vhost::ssl.
-
-The definition now wraps around apache::vhost::ssl
-for backward compatibility reasons.
-
-
-*/
 define apache::vhost-ssl (
   $ensure=present,
-  $config_file="",
+  $config_file='',
   $config_content=false,
   $htdocs=false,
   $conf=false,
   $readme=false,
   $docroot=false,
   $cgibin=true,
-  $user="",
+  $user='',
   $admin=$::admin,
-  $group="",
+  $group='',
   $mode=2570,
   $aliases=[],
-  $ip_address="*",
+  $ip_address='*',
   $cert=false,
   $certkey=false,
   $cacert=false,
@@ -33,15 +29,15 @@ define apache::vhost-ssl (
   $certcn=false,
   $verifyclient=undef,
   $options=[],
-  $days="3650",
+  $days='3650',
   $publish_csr=false,
   $sslonly=false,
   $ports=['*:80'],
   $sslports=['*:443'],
-  $accesslog_format="combined"
+  $accesslog_format='combined',
 ) {
 
-  warning "apache::vhost-ssl is deprecated. Use apache::vhost::ssl instead"
+  warning 'apache::vhost-ssl is deprecated. Use apache::vhost::ssl instead'
 
   apache::vhost::ssl{$name:
     ensure           => $ensure,
