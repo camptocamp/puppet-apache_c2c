c9ede2b75f063238efe5cff5209d94084facb33e
Marco Grunert <magomi@gmail.com>
2013-04-11 10:03:51 +0200

    Accept Ubuntu as operating system
     - Ubuntu uses the same paths for storing certificates
       as Debian

--- manifests/vhost/ssl.pp
+++ manifests/vhost/ssl.pp
@@ -180,7 +180,7 @@ define apache::vhost::ssl (
   } else {
     $cacertfile = $::operatingsystem ? {
       /RedHat|CentOS/ => '/etc/pki/tls/certs/ca-bundle.crt',
-      Debian          => '/etc/ssl/certs/ca-certificates.crt',
+      /Debian|Ubuntu/ => '/etc/ssl/certs/ca-certificates.crt',
     }
   }
 
