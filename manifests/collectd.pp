9222b70fd67e6b35d1492f303d09028d32d31b5f
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-05-30 10:33:35 +0200

    linted

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -1,22 +1,20 @@
-/*
-== Class: apache::collectd
-
-Configures collectd's apache plugin. This gathers data from apache's
-server-status and stores it in rrd files, from which you can make nice
-graphs.
-
-You will need collectd up and running, which can be cone using the
-puppet-collectd module.
-
-Requires:
-- Class["collectd"]
-
-Usage:
-  include apache
-  include collectd
-  include apache::collectd
-
-*/
+# == Class: apache::collectd
+#
+# Configures collectd's apache plugin. This gathers data from apache's
+# server-status and stores it in rrd files, from which you can make nice
+# graphs.
+#
+# You will need collectd up and running, which can be cone using the
+# puppet-collectd module.
+#
+# Requires:
+# - Class["collectd"]
+#
+# Usage:
+#   include apache
+#   include collectd
+#   include apache::collectd
+#
 class apache::collectd {
 
   # trick to check which collectd module we are using
@@ -31,9 +29,9 @@ class apache::collectd {
   </Instance>',
     }
   } else {
-    collectd::plugin { "apache":
+    collectd::plugin { 'apache':
       lines   => ['URL "http://localhost/server-status?auto"'],
-      require => Package["curl"],
+      require => Package['curl'],
     }
   }
 
