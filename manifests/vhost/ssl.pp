589e8607a8e47b7acdc05fbbdb4146b4b7968758
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-09-11 10:46:04 +0200

    Use $apache::ssl::root in apache::vhost::ssl

--- manifests/vhost/ssl.pp
+++ manifests/vhost/ssl.pp
@@ -160,7 +160,7 @@ define apache::vhost::ssl (
   }
 
   # used in ERB templates
-  $wwwroot = $apache::root
+  $wwwroot = $apache::ssl::root
   validate_absolute_path($wwwroot)
 
   $documentroot = $docroot ? {
