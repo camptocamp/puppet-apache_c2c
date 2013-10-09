4d1ffe7f9e3a8bc235da97821ec8b3df6f878d74
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 14:10:30 +0200

    Lint apache::aw-stats

--- manifests/aw-stats.pp
+++ manifests/aw-stats.pp
@@ -8,24 +8,26 @@ define apache::aw-stats($ensure=present, $aliases=[], $allowfullyearview=2) {
 
   file { "/etc/awstats/awstats.${name}.conf":
     ensure  => $ensure,
-    content => template("apache/awstats.erb"),
-    require => [Package["apache"], Class["apache::awstats"]],
+    content => template('apache/awstats.erb'),
+    require => [Package['apache'], Class['apache::awstats']],
   }
 
+  $source = $::operatingsystem ? {
+    /RedHat|CentOS/ => "puppet:///modules/${module_name}/awstats.rh.conf",
+    /Debian|Ubuntu/ => "puppet:///modules/${module_name}/awstats.deb.conf",
+  }
+  $seltype = $::operatingsystem ? {
+    'RedHat' => 'httpd_config_t',
+    'CentOS' => 'httpd_config_t',
+    default  => undef,
+  }
   file { "${wwwroot}/${name}/conf/awstats.conf":
     ensure  => $ensure,
     owner   => root,
     group   => root,
-    source  => $::operatingsystem ? {
-      /RedHat|CentOS/ => "puppet:///modules/${module_name}/awstats.rh.conf",
-      /Debian|Ubuntu/ => "puppet:///modules/${module_name}/awstats.deb.conf",
-    },
-    seltype => $::operatingsystem ? {
-      "RedHat" => "httpd_config_t",
-      "CentOS" => "httpd_config_t",
-      default  => undef,
-    },
-    notify  => Exec["apache-graceful"],
+    source  => $source,
+    seltype => $seltype,
+    notify  => Exec['apache-graceful'],
     require => Apache::Vhost[$name],
   }
 }
