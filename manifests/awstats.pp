b71e83a55e5ee5ff57eb508bcc75698ba5e9f3ce
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 14:12:32 +0200

    Lint apache::awstats

--- manifests/awstats.pp
+++ manifests/awstats.pp
@@ -1,31 +1,31 @@
 class apache::awstats {
 
-  package { "awstats":
+  package { 'awstats':
     ensure => installed
   }
 
   # ensure non-managed files are purged from directory
-  file {"/etc/awstats":
+  file {'/etc/awstats':
     ensure  => directory,
     source  => "puppet:///modules/${module_name}/etc/awstats",
-    mode    => 0755,
+    mode    => '0755',
     purge   => true,
     recurse => true,
     force   => true,
-    require => Package["awstats"],
+    require => Package['awstats'],
   }
 
   case $::operatingsystem {
 
     Debian,Ubuntu: {
-      cron { "update all awstats virtual hosts":
-        command => "/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null",
-        user    => "root",
+      cron { 'update all awstats virtual hosts':
+        command => '/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null',
+        user    => 'root',
         minute  => [0,10,20,30,40,50],
         require => Package[awstats]
       }
 
-      file { "/etc/cron.d/awstats":
+      file { '/etc/cron.d/awstats':
         ensure => absent,
       }
     }
@@ -34,23 +34,23 @@ class apache::awstats {
 
       # awstats RPM installs its own cron in /etc/cron.hourly/awstats
 
-      file { "/usr/share/awstats/wwwroot/cgi-bin/":
-        seltype => "httpd_sys_script_exec_t",
-        mode    => 0755,
+      file { '/usr/share/awstats/wwwroot/cgi-bin/':
+        seltype => 'httpd_sys_script_exec_t',
+        mode    => '0755',
         recurse => true,
-        require => Package["awstats"],
+        require => Package['awstats'],
       }
 
-      file { "/var/lib/awstats/":
-        seltype => "httpd_sys_script_ro_t",
+      file { '/var/lib/awstats/':
+        seltype => 'httpd_sys_script_ro_t',
         recurse => true,
-        require => Package["awstats"],
+        require => Package['awstats'],
       }
 
-      file { "/etc/httpd/conf.d/awstats.conf":
+      file { '/etc/httpd/conf.d/awstats.conf':
         ensure  => absent,
-        require => Package["awstats"],
-        notify  => Exec["apache-graceful"],
+        require => Package['awstats'],
+        notify  => Exec['apache-graceful'],
       }
     }
 
