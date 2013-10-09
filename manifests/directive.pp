877385e0a483c32524eacff83088773c0f124b3f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 14:44:47 +0200

    Lint apache::directive

--- manifests/directive.pp
+++ manifests/directive.pp
@@ -1,42 +1,44 @@
-/*
-
-== Definition: apache::directive
-
-Convenient wrapper around apache::conf resources to add random configuration
-snippets to an apache virtualhost.
-
-Parameters:
-- *ensure*: present/absent.
-- *directive*: apache directive(s) to be applied in the corresponding
-  <VirtualHost> section.
-- *vhost*: the virtualhost to which this directive will apply. Mandatory.
-- *filename*: basename of the file in which the directive(s) will be put.
-  Useful in the case directive order matters: apache reads the files in conf/
-  in alphabetical order.
-
-Requires:
-- Class["apache"]
-- matching Apache::Vhost[] instance
-
-Example usage:
-
-  apache::directive { "example 1":
-    ensure    => present,
-    directive => "
-      RewriteEngine on
-      RewriteRule ^/?$ https://www.example.com/
-    ",
-    vhost     => "www.example.com",
-  }
-
-  apache::directive { "example 2":
-    ensure    => present,
-    directive => content("example/snippet.erb"),
-    vhost     => "www.example.com",
-  }
-
-*/
-define apache::directive ($ensure="present", $directive="", $filename="", $vhost) {
+# == Definition: apache::directive
+#
+# Convenient wrapper around apache::conf resources to add random configuration
+# snippets to an apache virtualhost.
+#
+# Parameters:
+# - *ensure*: present/absent.
+# - *directive*: apache directive(s) to be applied in the corresponding
+#   <VirtualHost> section.
+# - *vhost*: the virtualhost to which this directive will apply. Mandatory.
+# - *filename*: basename of the file in which the directive(s) will be put.
+#   Useful in the case directive order matters: apache reads the files in conf/
+#   in alphabetical order.
+#
+# Requires:
+# - Class["apache"]
+# - matching Apache::Vhost[] instance
+#
+# Example usage:
+#
+#   apache::directive { "example 1":
+#     ensure    => present,
+#     directive => "
+#       RewriteEngine on
+#       RewriteRule ^/?$ https://www.example.com/
+#     ",
+#     vhost     => "www.example.com",
+#   }
+#
+#   apache::directive { "example 2":
+#     ensure    => present,
+#     directive => content("example/snippet.erb"),
+#     vhost     => "www.example.com",
+#   }
+#
+define apache::directive(
+  $vhost,
+  $ensure    = 'present',
+  $directive = '',
+  $filename  = '',
+) {
 
   $wwwroot = $apache::root
   validate_absolute_path($wwwroot)
