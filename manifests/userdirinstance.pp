04978f732ee6aa220390798d4275b152a9bb577c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 14:43:35 +0200

    Lint apache::userdirinstance

--- manifests/userdirinstance.pp
+++ manifests/userdirinstance.pp
@@ -1,16 +1,20 @@
-define apache::userdirinstance ($ensure=present, $vhost) {
+define apache::userdirinstance(
+  $vhost,
+  $ensure = present,
+) {
 
   $wwwroot = $apache::root
   validate_absolute_path($wwwroot)
 
+  $seltype = $::operatingsystem ? {
+    'RedHat' => 'httpd_config_t',
+    'CentOS' => 'httpd_config_t',
+    default  => undef,
+  }
   file { "${wwwroot}/${vhost}/conf/userdir.conf":
-    ensure => $ensure,
-    source => "puppet:///modules/${module_name}/userdir.conf",
-    seltype => $::operatingsystem ? {
-      "RedHat" => "httpd_config_t",
-      "CentOS" => "httpd_config_t",
-      default  => undef,
-    },
-    notify => Exec["apache-graceful"],
+    ensure  => $ensure,
+    source  => "puppet:///modules/${module_name}/userdir.conf",
+    seltype => $seltype,
+    notify  => Exec['apache-graceful'],
   }
 }
