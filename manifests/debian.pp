de7ef935722e898bd26e884951cb54af933a166c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 14:52:48 +0200

    Lint apache::debian

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -5,36 +5,36 @@ class apache::debian inherits apache::base {
   validate_absolute_path($wwwroot)
 
   # BEGIN inheritance from apache::base
-  Exec["apache-graceful"] {
-    command => "apache2ctl graceful",
-    onlyif => "apache2ctl configtest",
+  Exec['apache-graceful'] {
+    command => 'apache2ctl graceful',
+    onlyif => 'apache2ctl configtest',
   }
 
   # the following variables are used in template logrotate-httpd.erb
   $logrotate_paths = "${wwwroot}/*/logs/*.log ${apache::params::log}/*log"
-  $httpd_pid_file = "/var/run/apache2.pid"
-  $httpd_reload_cmd = "/etc/init.d/apache2 restart > /dev/null"
-  $awstats_condition = "-f /usr/share/doc/awstats/examples/awstats_updateall.pl -a -f /usr/lib/cgi-bin/awstats.pl"
-  $awstats_command = "/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null"
-  File["logrotate configuration"] {
-    path    => "/etc/logrotate.d/apache2",
+  $httpd_pid_file = '/var/run/apache2.pid'
+  $httpd_reload_cmd = '/etc/init.d/apache2 restart > /dev/null'
+  $awstats_condition = '-f /usr/share/doc/awstats/examples/awstats_updateall.pl -a -f /usr/lib/cgi-bin/awstats.pl'
+  $awstats_command = '/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null'
+  File['logrotate configuration'] {
+    path    => '/etc/logrotate.d/apache2',
     content => template("${module_name}/logrotate-httpd.erb"),
   }
 
-  File["default status module configuration"] {
-    path => "${apache::params::conf}/mods-available/status.conf",
+  File['default status module configuration'] {
+    path   => "${apache::params::conf}/mods-available/status.conf",
     source => "puppet:///modules/${module_name}/etc/apache2/mods-available/status.conf",
   }
   # END inheritance from apache::base
 
   $mpm_package = $apache_mpm_type ? {
-    "" => "apache2-mpm-prefork",
+    ''      => 'apache2-mpm-prefork',
     default => "apache2-mpm-${apache_mpm_type}",
   }
 
-  package { "${mpm_package}":
+  package { $mpm_package:
     ensure  => installed,
-    require => Package["apache"],
+    require => Package['apache'],
   }
 
   # directory not present in lenny
@@ -57,8 +57,8 @@ class apache::debian inherits apache::base {
 
   file { "${apache::params::conf}/conf.d/servername.conf":
     content => "ServerName ${::fqdn}\n",
-    notify  => Service["apache"],
-    require => Package["apache"],
+    notify  => Service['apache'],
+    require => Package['apache'],
   }
 
 }
