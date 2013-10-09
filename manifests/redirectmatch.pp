8dd080fc173268d215183ca834608ffae6f5ad99
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 14:08:33 +0200

    Lint apache::redirectmatch

--- manifests/redirectmatch.pp
+++ manifests/redirectmatch.pp
@@ -1,32 +1,29 @@
-/*
-
-== Definition: apache::redirectmatch
-
-Convenient way to declare a RedirectMatch directive in a virtualhost context.
-
-Parameters:
-- *ensure*: present/absent.
-- *regex*: regular expression matching the part of the URL which should get
-  redirected. Mandatory.
-- *url*: destination URL the redirection should point to. Mandatory.
-- *vhost*: the virtualhost to which this directive will apply. Mandatory.
-- *filename*: basename of the file in which the directive(s) will be put.
-  Useful in the case directive order matters: apache reads the files in conf/
-  in alphabetical order.
-
-Requires:
-- Class["apache"]
-- matching Apache::Vhost[] instance
-
-Example usage:
-
-  apache::redirectmatch { "example":
-    regex => "^/(foo|bar)",
-    url   => "http://foobar.example.com/",
-    vhost => "www.example.com",
-  }
-
-*/
+# == Definition: apache::redirectmatch
+#
+# Convenient way to declare a RedirectMatch directive in a virtualhost context.
+#
+# Parameters:
+# - *ensure*: present/absent.
+# - *regex*: regular expression matching the part of the URL which should get
+#   redirected. Mandatory.
+# - *url*: destination URL the redirection should point to. Mandatory.
+# - *vhost*: the virtualhost to which this directive will apply. Mandatory.
+# - *filename*: basename of the file in which the directive(s) will be put.
+#   Useful in the case directive order matters: apache reads the files in conf/
+#   in alphabetical order.
+#
+# Requires:
+# - Class["apache"]
+# - matching Apache::Vhost[] instance
+#
+# Example usage:
+#
+#   apache::redirectmatch { "example":
+#     regex => "^/(foo|bar)",
+#     url   => "http://foobar.example.com/",
+#     vhost => "www.example.com",
+#   }
+#
 define apache::redirectmatch (
   $regex,
   $url,
@@ -40,18 +37,20 @@ define apache::redirectmatch (
   $wwwroot = $apache::root
   validate_absolute_path($wwwroot)
 
+  $seltype = $::operatingsystem ? {
+    'RedHat' => 'httpd_config_t',
+    'CentOS' => 'httpd_config_t',
+    default  => undef,
+  }
+  $path = $filename ? {
+    ''      => "${wwwroot}/${vhost}/conf/redirect-${fname}.conf",
+    default => "${wwwroot}/${vhost}/conf/${filename}",
+  }
   file { "${name} redirect on ${vhost}":
     ensure  => $ensure,
     content => "# file managed by puppet\nRedirectMatch ${regex} ${url}\n",
-    seltype => $::operatingsystem ? {
-      'RedHat' => 'httpd_config_t',
-      'CentOS' => 'httpd_config_t',
-      default  => undef,
-    },
-    path    => $filename ? {
-      ''      => "${wwwroot}/${vhost}/conf/redirect-${fname}.conf",
-      default => "${wwwroot}/${vhost}/conf/${filename}",
-    },
+    seltype => $seltype,
+    path    => $path,
     notify  => Exec['apache-graceful'],
     require => Apache::Vhost[$vhost],
   }
