3567635bc70300693b47662113b4f9a7ee886b83
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-08-25 14:09:07 +0200

    Allow to use apache_c2c::deflate with puppetlabs backend

--- manifests/deflate.pp
+++ manifests/deflate.pp
@@ -6,17 +6,19 @@ class apache_c2c::deflate {
     ensure => present,
   }
 
-  file { 'deflate.conf':
-    ensure  => present,
-    path    => "${apache_c2c::params::conf}/conf.d/deflate.conf",
-    content => '# file managed by puppet
+  if $::apache_c2c::backend != 'puppetlabs' {
+    file { 'deflate.conf':
+      ensure  => present,
+      path    => "${apache_c2c::params::conf}/conf.d/deflate.conf",
+      content => '# file managed by puppet
 <IfModule mod_deflate.c>
   AddOutputFilterByType DEFLATE application/x-javascript application/javascript text/css
   BrowserMatch Safari/4 no-gzip
 </IfModule>
 ',
-    notify  => Exec['apache-graceful'],
-    require => Package['httpd'],
+      notify  => Exec['apache-graceful'],
+      require => Package['httpd'],
+    }
   }
 
 }
