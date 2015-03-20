bfe7d45929e252fba58ace3ed5904f4da6ecfdf5
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 10:11:54 +0100

    Fix unit tests for apache_c2c::namevhost

--- spec/defines/apache_namevhost_spec.rb
+++ spec/defines/apache_namevhost_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe 'apache_c2c::namevhost' do
-  let(:title) { '*:80' }
+  let(:pre_condition) do
+    "include ::apache_c2c"
+  end
+
+  let(:title) { '*:8080' }
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
       it { should contain_class('apache_c2c::params') }
@@ -17,11 +23,20 @@ describe 'apache_c2c::namevhost' do
             :ensure => e,
           } }
 
-          it { should contain_concat__fragment('apache-namevhost.conf-*:80').with(
-            'ensure'  => e,
-            'content' => "NameVirtualHost *:80\n",
-            'target'    => "#{VARS[os]['conf']}/ports.conf"
-          ) }
+          case facts[:osfamily]
+          when 'Debian'
+            it { should contain_concat__fragment('apache-namevhost.conf-*:8080').with(
+              'ensure'  => e,
+              'content' => "NameVirtualHost *:8080\n",
+              'target'  => '/etc/apache2/ports.conf',
+            ) }
+          else
+            it { should contain_concat__fragment('apache-namevhost.conf-*:8080').with(
+              'ensure'  => e,
+              'content' => "NameVirtualHost *:8080\n",
+              'target'  => '/etc/httpd/ports.conf',
+            ) }
+          end
         end
       end
     end
