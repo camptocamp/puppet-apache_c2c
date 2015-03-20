699c0f0b3a70809e81524cd6954360efad398f52
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 08:50:01 +0100

    Fix unit tests for apache_c2c::base::ssl

--- spec/classes/apache_base_ssl_spec.rb
+++ spec/classes/apache_base_ssl_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe 'apache_c2c::base::ssl' do
-  let(:pre_condition) { "include ::apache_c2c" }
+  let(:pre_condition) { "include ::apache_c2c::ssl" }
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
       it { should contain_apache_c2c__listen('443').with_ensure('present') }
