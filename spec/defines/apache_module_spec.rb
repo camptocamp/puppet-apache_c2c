a77928a6167b0ad317bd2ac06e75ef6451834ea3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 10:00:38 +0100

    Fix unit tests for apache_c2c::module

--- spec/defines/apache_module_spec.rb
+++ spec/defines/apache_module_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe 'apache_c2c::module' do
+  let(:pre_condition) do
+   "include ::apache_c2c"
+  end
+
   let(:title) { 'deflate' }
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
       describe 'ensuring presence' do
@@ -17,17 +23,24 @@ describe 'apache_c2c::module' do
         it { should contain_class('apache_c2c::params') }
 
         describe "using selinux" do
-          let (:facts) { {
-            :operatingsystem => os,
-            :osfamily        => os,
-            :selinux         => 'true',
-          } }
+          let (:facts) do
+            super().merge({
+              :selinux => true,
+            })
+          end
 
           it { should contain_apache_c2c__redhat__selinux('deflate') }
 
-          it do should contain_exec('a2enmod deflate').with(
-            'command' => "#{VARS[os]['a2enmod']} deflate"
-          ) end
+          case facts[:osfamily]
+          when 'Debian'
+            it { should contain_exec('a2enmod deflate').with(
+              'command' => '/usr/sbin//a2enmod deflate',
+            ) }
+          else
+            it { should contain_exec('a2enmod deflate').with(
+              'command' => '/usr/local/sbin//a2enmod deflate',
+            ) }
+          end
         end
       end
 
@@ -40,9 +53,16 @@ describe 'apache_c2c::module' do
 
         it { should_not contain_apache_c2c__redhat__selinux('deflate') }
 
-        it do should contain_exec('a2dismod deflate').with(
-          'command' => "#{VARS[os]['a2dismod']} deflate"
-        ) end
+        case facts[:osfamily]
+        when 'Debian'
+          it { should contain_exec('a2dismod deflate').with(
+            'command' => '/usr/sbin//a2dismod deflate',
+          ) }
+        else
+          it { should contain_exec('a2dismod deflate').with(
+            'command' => '/usr/local/sbin//a2dismod deflate',
+          ) }
+        end
       end
 
       describe 'using wrong value of ensure' do
