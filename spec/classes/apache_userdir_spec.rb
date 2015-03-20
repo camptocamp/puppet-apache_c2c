ac0d555a163e1e7428e3ce9090ea8765d3e9888d
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 10:17:18 +0100

    Fix unit tests for apache_c2c::userdir

--- spec/classes/apache_userdir_spec.rb
+++ spec/classes/apache_userdir_spec.rb
@@ -4,10 +4,16 @@ skel_directories = ['htdocs', 'conf', 'cgi-bin', 'private']
 
 describe 'apache_c2c::userdir' do
 
+  let(:pre_condition) do
+    "include ::apache_c2c"
+  end
+
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
       it { should contain_file('/etc/skel/public_html').with_ensure('directory') }
