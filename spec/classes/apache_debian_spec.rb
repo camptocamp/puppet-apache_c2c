e6e07baeabc7417a7825d347662a0afdfe5b26d9
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 08:56:50 +0100

    Fix unit tests for apache_c2c::debian

--- spec/classes/apache_debian_spec.rb
+++ spec/classes/apache_debian_spec.rb
@@ -3,37 +3,55 @@ require 'spec_helper'
 describe 'apache_c2c::debian' do
   let(:pre_condition) { "include ::apache_c2c" }
 
-  let(:node) { "myhost.com" }
-
   on_supported_os.select { |os| os =~ /debian/ }.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
       it { should contain_class('apache_c2c::params') }
 
       describe 'using default values' do
+        let(:root) do
+          case facts[:osfamily]
+          when 'Debian'
+            '/var/www'
+          else
+            '/var/www/vhosts'
+          end
+        end
+
+        let(:conf) do
+          case facts[:osfamily]
+          when 'Debian'
+            '/etc/apache2'
+          else
+            '/etc/httpd'
+          end
+        end
+
         it { should contain_package('apache2-mpm-prefork').with_ensure('installed') }
 
-        it do should contain_file("#{VARS[os]['root']}/apache2-default").with(
+        it do should contain_file("#{root}/apache2-default").with(
           'ensure' => 'absent',
           'force'  => 'true'
         ) end
 
-        it { should contain_file("#{VARS[os]['root']}/index.html").with_ensure('absent') }
+        it { should contain_file("#{root}/index.html").with_ensure('absent') }
 
-        it { should contain_file("#{VARS[os]['root']}/html").with_ensure('directory') }
+        it { should contain_file("#{root}/html").with_ensure('directory') }
 
-        it do should contain_file("#{VARS[os]['root']}/html/index.html").with(
-          'ensure'  => 'present',
+        it do should contain_file("#{root}/html/index.html").with(
+          'ensure'  => 'file',
           'owner'   => 'root',
           'group'   => 'root',
           'mode'    => '0644',
           'content' => "<html><body><h1>It works!</h1></body></html>\n"
         ) end
 
-        it { should contain_file("#{VARS[os]['conf']}/conf.d/servername.conf").with_content("ServerName myhost.com\n") }
+        it { should contain_file("#{conf}/conf.d/servername.conf").with_content("ServerName foo.example.com\n") }
       end
     end
   end
