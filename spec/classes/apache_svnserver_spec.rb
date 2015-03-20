4d30ae1fe24db845d6b0d8e3c790f13a56f0bf9f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 09:52:24 +0100

    Fix unit tests for apache_c2c::svnserver

--- spec/classes/apache_svnserver_spec.rb
+++ spec/classes/apache_svnserver_spec.rb
@@ -5,10 +5,17 @@ describe 'apache_c2c::svnserver' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
-      it { should contain_package(VARS[os]['mod_svn']).with_ensure('present') }
+      case facts[:osfamily]
+      when 'Debian'
+        it { should contain_package('libapache2-svn').with_ensure('present') }
+      else
+        it { should contain_package('mod_dav_svn').with_ensure('present') }
+      end
 
       it { should contain_apache_c2c__module('dav').with_ensure('present') }
       it { should contain_apache_c2c__module('dav_svn').with_ensure('present') }
