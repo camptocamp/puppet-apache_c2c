fee3bc4bb65a7e5c2e2c10ce6162e552617ef21c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 10:10:35 +0100

    Fix unit tests for apache_c2c::listen

--- spec/defines/apache_listen_spec.rb
+++ spec/defines/apache_listen_spec.rb
@@ -1,21 +1,36 @@
 require 'spec_helper'
 
 describe 'apache_c2c::listen' do
-  let(:title) { '80' }
+  let(:pre_condition) do
+    "include ::apache_c2c"
+  end
+
+  let(:title) { '8080' }
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
       it { should contain_class('apache_c2c::params') }
 
-      it do should contain_concat__fragment('apache-ports.conf-80').with(
-        'ensure'  => 'present',
-        'content' => "Listen 80\n",
-        'target'  => "#{VARS[os]['conf']}/ports.conf"
-      ) end
+      case facts[:osfamily]
+      when 'Debian'
+        it { should contain_concat__fragment('apache-ports.conf-8080').with(
+          'ensure'  => 'present',
+          'content' => "Listen 8080\n",
+          'target'  => '/etc/apache2/ports.conf',
+        ) }
+      else
+        it { should contain_concat__fragment('apache-ports.conf-8080').with(
+          'ensure'  => 'present',
+          'content' => "Listen 8080\n",
+          'target'  => '/etc/httpd/ports.conf',
+        ) }
+      end
     end
   end
 end
