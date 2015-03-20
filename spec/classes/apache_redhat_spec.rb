207b6f1cfdf7ceb9819cee2f55f9b2c79b2e4807
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-20 09:47:36 +0100

    Fix unit tests on apache_c2c::redhat

--- spec/classes/apache_redhat_spec.rb
+++ spec/classes/apache_redhat_spec.rb
@@ -2,68 +2,70 @@ require 'spec_helper'
 
 describe 'apache_c2c::redhat' do
   let(:pre_condition) { "include ::apache_c2c" }
-  ['RedHat'].each do |os|
-    let(:facts) { {
-      :concat_basedir    => '/foo',
-      :operatingsystem   => os,
-      :osfamily          => os,
-      :lsbmajdistrelease => '5',
-    } }
 
-    it { should contain_class('apache_c2c::params') }
+  on_supported_os.select { |os| os =~ /redhat/ }.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
+      end
 
-    ['a2ensite', 'a2dissite', 'a2enmod', 'a2dismod'].each do |script|
-      it { should contain_file("/usr/local/sbin/#{script}").with(
-        'ensure' => 'present',
-        'mode'   => '0755',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'source' => 'puppet:///modules/apache_c2c/usr/local/sbin/a2X.redhat'
+      let(:conf) { '/etc/httpd' }
+      let(:conf_seltype) { 'httpd_config_t' }
+
+      it { should contain_class('apache_c2c::params') }
+
+      ['a2ensite', 'a2dissite', 'a2enmod', 'a2dismod'].each do |script|
+        it { should contain_file("/usr/local/sbin/#{script}").with(
+          {
+            'ensure' => 'file',
+            'mode'   => '0755',
+            'owner'  => 'root',
+            'group'  => 'root',
+            'source' => 'puppet:///modules/apache_c2c/usr/local/sbin/a2X.redhat'
+          }
+        ) }
+      end
+
+      it { should contain_augeas('select httpd mpm httpd').with_changes('set /files/etc/sysconfig/httpd/HTTPD /usr/sbin/httpd') }
+
+      ['sites-available', 'sites-enabled', 'mods-enabled'].each do |dir|
+        it { should contain_file("#{conf}/#{dir}").with(
+          {
+            'ensure'  => 'directory',
+            'mode'    => '0755',
+            'owner'   => 'root',
+            'group'   => 'root',
+            'seltype' => conf_seltype,
+          }
+        ) }
+      end
+
+      it { should contain_file("#{conf}/conf/httpd.conf").with(
+        {
+          'ensure'  => 'file',
+          'seltype' => conf_seltype,
+        }
       ) }
-    end
 
-    it { should contain_augeas('select httpd mpm httpd').with_changes('set /files/etc/sysconfig/httpd/HTTPD /usr/sbin/httpd') }
-
-    ['sites-available', 'sites-enabled', 'mods-enabled'].each do |dir|
-      it { should contain_file("#{VARS[os]['conf']}/#{dir}").with(
-        'ensure'  => 'directory',
-        'mode'    => '0755',
-        'owner'   => 'root',
-        'group'   => 'root',
-        'seltype' => VARS[os]['conf_seltype']
-      ) }
-    end
-
-    it { should contain_file("#{VARS[os]['conf']}/conf/httpd.conf").with(
-      'ensure'  => 'present',
-      'seltype' => VARS[os]['conf_seltype']
-    ) }
-
-    ['5', '6'].each do |release|
-      describe "with lsbmajdistrelease #{release}" do
-        let(:facts) { {
-          :concat_basedir    => '/foo',
-          :operatingsystem   => os,
-          :osfamily          => os,
-          :lsbmajdistrelease => release,
-        } }
-
-        it { should contain_file("#{VARS[os]['conf']}/mods-available").with(
+      it { should contain_file("#{conf}/mods-available").with(
+        {
           'ensure'  => 'directory',
-          'source'  => "puppet:///modules/apache_c2c/etc/httpd/mods-available/redhat#{release}/",
+          'source'  => "puppet:///modules/apache_c2c/etc/httpd/mods-available/redhat#{facts[:operatingsystemmajrelease]}/",
           'recurse' => 'true',
           'mode'    => '0755',
           'owner'   => 'root',
           'group'   => 'root',
-          'seltype' => VARS[os]['conf_seltype']
-        ) }
-      end
+          'seltype' => conf_seltype,
+        }
+      ) }
+
+      it { should contain_apache_c2c__module('log_config').with_ensure('present') }
+
+      it { should contain_file('/var/www/cgi-bin').with_ensure('absent') }
+
+      it { should contain_file("#{conf}/conf.d/proxy_ajp.conf").with_ensure('file').with_content(/# File managed by puppet/) }
     end
-
-    it { should contain_apache_c2c__module('log_config').with_ensure('present') }
-
-    it { should contain_file('/var/www/cgi-bin').with_ensure('absent') }
-
-    it { should contain_file("#{VARS[os]['conf']}/conf.d/proxy_ajp.conf").with_ensure('present').with_content(/# File managed by puppet/) }
   end
 end
